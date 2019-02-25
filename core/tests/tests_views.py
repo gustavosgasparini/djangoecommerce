393b692585e4041bbfcf5636aa98d52f6a7dc42e
Gustavo Gasparini <gustavoproga@gmail.com>
2019-02-25 11:04:54 -0300

    removendo comentarios

---
+++ core/tests/tests_views.py
@@ -0,0 +1,38 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+# Create your tests here.
+class IndexViewTestCase(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('index')
+    
+    def tearDown(self):
+        pass
+    
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'index.html')
+
+
+class ContactViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('contact')
+
+    def tearDown(self):
+        pass
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'contact.html')
