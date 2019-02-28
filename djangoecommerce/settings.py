0f3e411709fef92527bbf60168085b7e05fe2ae3
Gustavo Gasparini <gustavoproga@gmail.com>
2019-02-28 10:34:39 -0300

    ignore

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -130,11 +130,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
